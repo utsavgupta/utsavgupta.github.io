ee8fcd9add5be981f365b4323216346684e9e499
utsavgupta <utsavgupta@users.noreply.github.com>
2021-10-23 17:08:38 +0000

    deploy: 6c1f419c132bc0a232c365c219c7108d8cfed2e5

---
+++ js/app.min.bcc439d23d863a220cd260d557c53845681e42b052123f0540b0f06c4d4e6a076c675f13c1beef6c293a8d7c26681a092feb6296455c725cd0c1caf4ba1bd074.js
@@ -0,0 +1,7 @@
+function resizeProgressBar(){let progressbar=document.querySelector("#progress-bar")
+let mainsection=document.querySelector("main")
+let scrollDistance=mainsection.getBoundingClientRect().top
+let progresswidth=(scrollDistance/(document.documentElement.clientHeight-mainsection.getBoundingClientRect().height))*100
+if(progresswidth<0){progresswidth=0}
+progressbar.style.width=progresswidth+"%"}
+window.addEventListener("scroll",resizeProgressBar)
