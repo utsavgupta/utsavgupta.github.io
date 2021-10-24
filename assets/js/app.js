8c252a9635dd3bad0fb1232e751f20200dcbf51f
Utsav Gupta <mail@utsavgupta.in>
2021-10-24 18:57:37 +0530

    add button to go to top

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,31 @@
-function resizeProgressBar() {
-    let progressbar = document.querySelector("#progress-bar")
-    let mainsection = document.querySelector("main")
+const progressbar = document.querySelector("#progress-bar")
+const main = document.querySelector("main")
+const mainsection = document.querySelector("section.main-content")
+const topbutton = document.querySelector("#top-button")
 
-    let scrollDistance = mainsection.getBoundingClientRect().top
-    let progresswidth = (scrollDistance / (document.documentElement.clientHeight - mainsection.getBoundingClientRect().height)) * 100
+function resizeProgressBar() {
+    let scrollDistance = main.getBoundingClientRect().top
+    let progresswidth = (scrollDistance / (document.documentElement.clientHeight - main.getBoundingClientRect().height)) * 100
 
     if (progresswidth < 0) {
         progresswidth = 0
     }
 
+    if (progresswidth == 0) {
+        topbutton.style.display = "none"
+    } else {
+        topbutton.style.display = "block"
+    }
+
     progressbar.style.width = progresswidth + "%"
 }
 
+function positionTopButton() {
+    let rightOfMain = main.getBoundingClientRect().right;
+    topbutton.style.left = (rightOfMain - topbutton.getBoundingClientRect().width) + "px"
+}
+
+positionTopButton()
+
 window.addEventListener("scroll", resizeProgressBar)
+window.addEventListener("resize", positionTopButton)
